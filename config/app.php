8fab6cf6013449a80648bfbcc009104a37b3ea8c
Enric Bisbe Gil <enricu@gmail.com>
2016-04-28 19:19:10 +0200

    Add mongo plugin

--- config/app.php
+++ config/app.php
@@ -156,6 +156,10 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+         * Bikebitants services
+         */
+        Jenssegers\Mongodb\MongodbServiceProvider::class,
     ],
 
     /*
