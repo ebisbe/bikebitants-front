71335f7e432f7ce0b4504abfab583f34d567b77e
Enric Bisbe Gil <enricu@gmail.com>
2016-05-19 18:31:22 +0200

    Gulp modification

--- gulpfile.js
+++ gulpfile.js
@@ -13,6 +13,7 @@ var elixir = require('laravel-elixir');
 
 elixir(function (mix) {
     mix
+        //js
         .copy('vendor/bower_components/jquery/dist/jquery.js', 'resources/assets/js/jquery.js')
         .copy('vendor/bower_components/jquery-ui/jquery-ui.js', 'resources/assets/js/jquery-ui.js')
         .copy('vendor/bower_components/google-maps/lib/Google.js', 'resources/assets/js/google-maps.js')
@@ -22,6 +23,15 @@ elixir(function (mix) {
         .copy('vendor/bower_components/dragtable/jquery.dragtable.js', 'resources/assets/js/jquery.dragtable.js')
         .copy('vendor/bower_components/owl.carousel/dist/owl.carousel.js', 'resources/assets/js/owl.carousel.js')
         .copy('vendor/bower_components/jquery.mb.ytplayer/dist/jquery.mb.YTPlayer.js', 'resources/assets/js/jquery.mb.YTPlayer.js')
+
+        //css
+        .copy('vendor/bower_components/bootstrap/dist/css/bootstrap.css', 'resources/assets/css/bootstrap.css')
+        .copy('vendor/bower_components/jquery-ui/themes/smoothness/jquery-ui.css', 'resources/assets/css/jquery-ui.css')
+        .copy('vendor/bower_components/font-awesome/css/font-awesome.css', 'resources/assets/css/font-awesome.css')
+        .copy('vendor/bower_components/font-awesome/fonts', 'public/build/fonts')
+        .copy('vendor/bower_components/dragtable/dragtable.css', 'resources/assets/css/dragtable.css')
+        .copy('vendor/bower_components/owl.carousel/dist/assets/owl.carousel.css', 'resources/assets/css/owl.carousel.css')
+        .copy('vendor/bower_components/animate.css/animate.css', 'resources/assets/css/animate.css')
 
         .scripts([
             'jquery.js',
@@ -38,5 +48,17 @@ elixir(function (mix) {
             'custom.js',
             'shop.js'
         ])
-        .version(['js/all.js']);
+
+        .styles([
+            'bootstrap.css',
+            'jquery-ui.css',
+            'font-awesome.css',
+            'dragtable.css',
+            'owl.carousel.css',
+            'animate.css',
+            'custom.css',
+            'color/green.css'
+        ])
+
+        .version(['js/all.js', 'css/all.css']);
 });
