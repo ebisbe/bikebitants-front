112934135e15aab276ae439639a170607aaa3382
Enric Bisbe Gil <enricu@gmail.com>
2016-09-01 12:24:35 +0200

    Add a few new menus

--- resources/views/partials/menu.blade.php
+++ resources/views/partials/menu.blade.php
@@ -1,26 +1,33 @@
-        <!-- Main navigation -->
+<!-- Main navigation -->
 <div class="sidebar-category sidebar-category-visible">
     <div class="category-content no-padding">
         <ul class="navigation navigation-main navigation-accordion">
-            <li >
+            <li>
                 <a href="{{ route('admin.dashboard') }}"><i class="icon-home2"></i> <span>Dashboard</span></a>
             </li>
-            <!-- /main
+
             <li class="navigation-header">
-                <span>Reports</span>
+                <span>Commerce</span>
                 <i class="icon-menu" title="Main pages"></i>
             </li>
 
-            <li>
-                <a href="/reports/tracing"><i class="icon-vector2"></i> <span>Tracing reports</span></a>
+            <li><a href="{{ route('brand.index') }}">
+                    <i class="glyphicon glyphicon-tags"></i><span>Brands</span></a>
             </li>
+            <li><a href="{{ route('coupon.index') }}">
+                    <i class="glyphicon glyphicon-gift"></i><span>Coupons</span></a>
+            </li>
+            <li><a href="{{ route('lead.index') }}">
+                    <i class="glyphicon glyphicon-magnet"></i><span>Leads</span></a>
+            </li>
+
             <li class="navigation-header">
-                <span>Tools</span>
+                <span>Others</span>
                 <i class="icon-menu" title="Users"></i>
             </li>
-            <li><a href="/offline-actions"><span>Offline Actions</span></a></li>
-            -->
+            <li><a href="{{ route('country.index') }}">
+                    <i class="glyphicon glyphicon-globe"></i><span>Countries</span></a>
+            </li>
         </ul>
     </div>
 </div>
-<!-- /main navigation -->
