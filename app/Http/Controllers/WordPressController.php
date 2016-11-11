6c209c0c074a9e0bd5694e185d03fe40fbf74c35
Enric Bisbe Gil <enricu@gmail.com>
2016-11-11 17:37:07 +0100

    Fix wpController

--- app/Http/Controllers/WordPressController.php
+++ app/Http/Controllers/WordPressController.php
@@ -8,8 +8,8 @@ use App\Http\Requests;
 
 class WordPressController extends Controller
 {
-    public function index()
+    public function show($command, Request $request)
     {
-        dd(\Woocommerce::get('', ['page' => 1]));
+        dd(\Woocommerce::get($command, ['page' => $request->get('page', 1), 'search' => $request->get('search', '')]));
     }
 }
