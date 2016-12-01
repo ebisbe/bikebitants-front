fb9d879422d40e06e9bacfbb1945b4189485a06d
Enric Bisbe Gil <enricu@gmail.com>
2016-12-01 07:39:10 +0100

    Add js and css files

--- resources/views/layouts/shop.blade.php
+++ resources/views/layouts/shop.blade.php
@@ -123,7 +123,7 @@
                         </ul>
                     </li>--}}
 
-                    @include('partials.menu_cart')
+                    <menu-cart></menu-cart>
                     {{--<li class="dropdown navbar-search hidden-xs">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-search"></i></a>
                         <ul class="dropdown-menu">
@@ -278,7 +278,7 @@
 <!-- ==========================
  JS
 =========================== -->
-<script src="{{ elixir('js/all.js') }}"></script>
+<script src="{{ elixir('js/app.js') }}"></script>
 @stack('footer.scripts')
 <!--[if lt IE 9]>
 <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
