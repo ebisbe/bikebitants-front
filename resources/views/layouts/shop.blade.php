dfa76d6dc604a6bb2598211fbbf6f74dca47447d
Enric Bisbe Gil <enricu@gmail.com>
2016-12-12 00:08:04 +0100

    Moved wrong line

--- resources/views/layouts/shop.blade.php
+++ resources/views/layouts/shop.blade.php
@@ -8,7 +8,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset="utf-8">
 
-    <script src="{{ elixir('js/app.js') }}"></script>
 
     <title>{{ MetaTag::get('title') }}</title>
 
@@ -287,12 +286,13 @@
  JS
 =========================== -->
 <link rel="stylesheet" href="{{ elixir('css/all.css') }}">
+<script src="{{ elixir('js/app.js') }}" async></script>
+
 @stack('footer.scripts')
 <!--[if lt IE 9]>
 <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
 <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
 <![endif]-->
 
-@include('partials.flash_message')
 </body>
 </html>
