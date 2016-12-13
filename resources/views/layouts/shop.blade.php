30cdb5e3c3fcd83b78d34f9d34fd852211aba574
Enric Bisbe Gil <enricu@gmail.com>
2016-12-13 19:28:19 +0100

    Dont inject

--- resources/views/layouts/shop.blade.php
+++ resources/views/layouts/shop.blade.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset="utf-8">
 
-    @injectCss('assets/css/injected.css')
+    {{--@injectCss('assets/css/injected.css')--}}
 
     <title>{{ MetaTag::get('title') }}</title>
 
