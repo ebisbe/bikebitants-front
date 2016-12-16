754c35726f2562cd65cd59c54ca5ef97621b54e8
Enric Bisbe Gil <enricu@gmail.com>
2016-12-16 13:24:26 +0100

    Twitter fail silently

--- app/Business/Services/TwitterService.php
+++ app/Business/Services/TwitterService.php
@@ -7,10 +7,19 @@ use \Cache;
 
 class TwitterService
 {
+    /**
+     * @param string $user
+     * @param int $number
+     * @return array|mixed
+     */
     public function getLastsTweets($user = 'bikebitants', $number = 2)
     {
-        return Cache::remember("tweets_{$user}_{$number}", 60, function () use ($number, $user) {
-            return Twitter::getUserTimeline(['screen_name' => $user, 'count' => $number]);
-        });
+        try {
+            return Cache::remember("tweets_{$user}_{$number}", 60, function () use ($number, $user) {
+                return Twitter::getUserTimeline(['screen_name' => $user, 'count' => $number]);
+            });
+        } catch (\Exception $e) {
+            return [];
+        }
     }
 }
