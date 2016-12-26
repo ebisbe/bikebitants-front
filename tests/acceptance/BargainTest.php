0036adf6c093671f6af55310692c23eda073cd73
Enric Bisbe Gil <enricu@gmail.com>
2016-12-26 09:05:56 +0100

    Fix BargainTest

--- tests/acceptance/BargainTest.php
+++ tests/acceptance/BargainTest.php
@@ -1,16 +1,26 @@
 <?php
 
+use App\Product;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class BargainTest extends TestCase
 {
+    use DatabaseMigrations;
+
     /** @test */
     public function view_bargains()
     {
-        $this->visit(route('shop.bargain'))
-            ->see('Variable Product 2')
+        //arrange
+        factory(Product::class)->states('bargain')->create(['name' => 'Variable Product 2']);
+        factory(Product::class)->create(['name' => 'Simple Product']);
+
+        //act
+        $this->visit(route('shop.bargain'));
+
+        //assert
+        $this->see('Variable Product 2')
             ->see('Oferta Bikebitants')
             ->see('layout.shop')
             ->see('bargain.title')
