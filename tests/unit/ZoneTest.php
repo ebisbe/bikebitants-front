c840e018dd9a7cdd204be593473f74eb64503efb
Enric Bisbe Gil <enricu@gmail.com>
2016-12-26 08:56:09 +0100

    Fix ZoneTest

--- tests/unit/ZoneTest.php
+++ tests/unit/ZoneTest.php
@@ -1,13 +1,18 @@
 <?php
 
 use App\Zone;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 
-class ZoneTest extends TestCase {
+class ZoneTest extends TestCase
+{
+
+    use DatabaseMigrations;
 
     /** @test */
     public function get_zone_from_state()
     {
         //arrange
+        //Data is in migrations
 
         //act
         $zone = Zone::inState('B')->first();
