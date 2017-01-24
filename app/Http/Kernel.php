b471ad2226e1ea01348cb5049cc8b7a4f84cbb03
Laravel Shift <shift@laravelshift.com>
2017-01-24 10:11:53 +0000

    Shift Middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -29,12 +29,14 @@ class Kernel extends HttpKernel
             \Illuminate\Session\Middleware\StartSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Spatie\CookieConsent\CookieConsentMiddleware::class,
         ],
 
         'api' => [
             'throttle:60,1',
+            'bindings',
             'bindings',
         ],
     ];
@@ -47,8 +49,8 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $routeMiddleware = [
-        'auth' => \App\Http\Middleware\Authenticate::class,
-        'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
+        'auth' => \Illuminate\Auth\Middleware\Authenticate::class,
+        'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
