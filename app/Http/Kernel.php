f49830f862202bb37802c16327c2aa7bb7546e5d
Enric Bisbe Gil <enricu@gmail.com>
2017-01-24 13:24:21 +0100

    Review routes

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -29,14 +29,12 @@ class Kernel extends HttpKernel
             \Illuminate\Session\Middleware\StartSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Spatie\CookieConsent\CookieConsentMiddleware::class,
         ],
 
         'api' => [
             'throttle:60,1',
-            'bindings',
             'bindings',
         ],
     ];
@@ -50,7 +48,6 @@ class Kernel extends HttpKernel
      */
     protected $routeMiddleware = [
         'auth' => \Illuminate\Auth\Middleware\Authenticate::class,
-        'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -11,7 +11,5 @@ class VerifyCsrfToken extends BaseVerifier
      *
      * @var array
      */
-    protected $except = [
-        'shop.callback'
-    ];
+    protected $except = [];
 }
