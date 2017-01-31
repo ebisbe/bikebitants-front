4ecce00840d0e400f8a6f025419838fb56fd042a
Enric Bisbe Gil <enricu@gmail.com>
2017-01-31 13:42:58 +0100

    Use webpack instead of gulpfile.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,4 @@
-let mix = require('laravel-mix').mix;
+const {mix} = require('laravel-mix');
 
 /*
  |--------------------------------------------------------------------------
@@ -11,5 +11,31 @@ let mix = require('laravel-mix').mix;
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+mix
+    .js('resources/assets/js/app.js', 'public/js')
+
+    //.copy('public/css/bower/font-awesome/fonts', 'public/fonts')
+    //.copy('public/css/bower/sourceSansPro/', 'public/css')
+    //.copy('public/css/bower/raleway/', 'public/css')
+
+    /*.copy('resources/assets/images', 'public/css/images')
+
+    .copy('resources/assets/css/custom.css', 'public/css')
+    .copy('resources/assets/css/bikebitants.css', 'public/css')
+    .copy('resources/assets/css/color/bikebitants_green.css', 'public/css')
+
+    .combine([
+        'public/css/bower/bootstrap/dist/css/bootstrap.css',
+        'custom.css',
+        'bikebitants.css',
+        'bikebitants_green.css',
+        'public/css/bower/jquery-ui/themes/smoothness/jquery-ui.css',
+        'public/css/bower/font-awesome/css/font-awesome.css',
+        'node_modules/owl.carousel/dist/owl.carousel.css',
+        'public/css/bower/animate.css/animate.css',
+    ], 'public/css/all.css')*/
+
+    .sass('resources/assets/css/app.scss', 'public/css/app.css')
+    .sass('resources/assets/css/vendor.scss', 'public/css/vendor.css')
+
+    .version();
