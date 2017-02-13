d2e8ea644c1334c7026d267448b97f6f07c4011c
Enric Bisbe Gil <enricu@gmail.com>
2017-02-13 13:21:22 +0100

    Share queue worker file

--- deploy.php
+++ deploy.php
@@ -6,7 +6,7 @@ require 'recipe/laravel.php';
 
 set('repository', 'git@bitbucket.org:bikebitants/bikebitants.git');
 
-add('shared_files', ['database/geoip/GeoLite2-City.mmdb']);
+add('shared_files', ['database/geoip/GeoLite2-City.mmdb', 'laravel-worker.conf']);
 add('shared_dirs', ['public/img']);
 
 add('writable_dirs', []);
