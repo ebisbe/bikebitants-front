63252e2c01d9930870cd19349aa782e5cd1fed0e
Enric Bisbe Gil <enricu@gmail.com>
2017-03-06 09:08:46 +0100

    Float label again in catalgoue and change font sizes too

--- resources/views/partials/catalogue_product.blade.php
+++ resources/views/partials/catalogue_product.blade.php
@@ -20,12 +20,12 @@
             </div>
             <div class="col-sm-9">
                 <div class="product-body">
+                    @include('partials.labels')
                     <h1>
                         <a href="{{ route('shop.slug', ['slug' => $product->slug]) }}">
                             {{ str_limit($product->name, 29) }}
                         </a>
                     </h1>
-                    @include('partials.labels')
                     @include('partials.product_rating', ['rating' => $product->rating, 'total_reviews' => count($product->reviews)])
                     @include('partials.price', ['class' => 'blocked'])
                     <div class="p">{!! $product->introduction !!}</div>
