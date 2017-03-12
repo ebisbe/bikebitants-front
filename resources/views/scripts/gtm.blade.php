1c0d8b123887790810ca77ba49ae3d7a37b393ea
Enric Bisbe Gil <enricu@gmail.com>
2017-03-12 23:46:21 +0100

    Add analytics for production

--- resources/views/scripts/gtm.blade.php
+++ resources/views/scripts/gtm.blade.php
@@ -17,6 +17,21 @@
             f.parentNode.insertBefore(j, f);
         })(window, document, 'script', 'dataLayer', 'GTM-NQNB3K');</script>
     <!-- End Google Tag Manager -->
+    <script>
+        (function (i, s, o, g, r, a, m) {
+            i['GoogleAnalyticsObject'] = r;
+            i[r] = i[r] || function () {
+                    (i[r].q = i[r].q || []).push(arguments)
+                }, i[r].l = 1 * new Date();
+            a = s.createElement(o),
+                m = s.getElementsByTagName(o)[0];
+            a.async = 1;
+            a.src = g;
+            m.parentNode.insertBefore(a, m)
+        })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+        ga('create', 'UA-67842613-1', 'auto');
+        ga("require", "ec");
+    </script>
 @else
     <script>
         (function (i, s, o, g, r, a, m) {
