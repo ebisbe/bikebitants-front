3afef9118bfe3bc7f9025f25d376b1149cb71d3b
Enric Bisbe Gil <enricu@gmail.com>
2017-03-15 09:24:48 +0100

    Update footer in checkout and error layouts. Fix url contact

---
+++ resources/views/layouts/partials/footer.blade.php
@@ -0,0 +1,17 @@
+<div class="row">
+    <div class="col-sm-6">
+        <p class="copyright">&copy; Bikebitants SL 2015</p>
+    </div>
+    <div class="col-sm-6">
+        <ul class="list-inline payment-methods">
+            {{--<li><i class="fa fa-cc-amex"></i></li>--}}
+            {{--<li><i class="fa fa-cc-diners-club"></i></li>--}}
+            {{--<li><i class="fa fa-cc-discover"></i></li>
+            <li><i class="fa fa-cc-jcb"></i></li>--}}
+            <li><i class="fa fa-cc-mastercard"></i></li>
+            <li><i class="fa fa-cc-paypal"></i></li>
+            {{--<li><i class="fa fa-cc-stripe"></i></li>--}}
+            <li><i class="fa fa-cc-visa"></i></li>
+        </ul>
+    </div>
+</div>
