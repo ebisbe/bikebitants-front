c201125eaf8da2b7dbd6f504a4781cac53f8c6e4
Enric Bisbe Gil <enricu@gmail.com>
2017-03-15 09:13:15 +0100

    Add entity options to command

--- app/Console/Commands/WordpressSync.php
+++ app/Console/Commands/WordpressSync.php
@@ -4,6 +4,7 @@ namespace App\Console\Commands;
 
 use App\Business\Services\WordpressService;
 use Illuminate\Console\Command;
+use Illuminate\Support\Collection;
 use Pixelpeter\Woocommerce\Facades\Woocommerce;
 
 class WordpressSync extends Command
@@ -13,7 +14,7 @@ class WordpressSync extends Command
      *
      * @var string
      */
-    protected $signature = 'wp:sync';
+    protected $signature = 'wp:sync {--entity=*}';
 
     /**
      * The console command description.
@@ -24,6 +25,7 @@ class WordpressSync extends Command
 
     /** @var  WordpressService $wordpressService */
     protected $wordpressService;
+
     /**
      * Create a new command instance.
      *
@@ -41,28 +43,62 @@ class WordpressSync extends Command
     {
         $this->wordpressService = $wordpressService;
 
-        $this->info('Sync Customer:');
+        $entities = $this->getEntitiesToSync();
+        $entities->each(function ($name) {
+            $method_name = "sync" . ucfirst($name);
+
+            if (method_exists($this, $method_name)) {
+                $this->info("Sync $name:");
+                $this->$method_name();
+                $this->info('');
+            } else {
+                $this->error("Entity $name not found.");
+            }
+        });
+    }
+
+    /**
+     * By default returns all entities to sync
+     * @return Collection
+     */
+    protected function getEntitiesToSync(): Collection
+    {
+        $entities = $this->option('entity');
+
+        if (!empty($entities)) {
+            return collect($entities);
+        }
+
+        return collect(['Customer', 'Product', 'Category', 'Tax', 'Coupon']);
+    }
+
+    protected function syncCustomer()
+    {
         $this->wordpressService->setWooCommerceCallback('customers');
         $this->wordpressService->import();
-        $this->info('');
+    }
 
-        $this->info('Sync Category:');
+    protected function syncCategory()
+    {
         $this->wordpressService->setWooCommerceCallback('products/categories');
         $this->wordpressService->setWordpressServiceCallback('syncCategory');
         $this->wordpressService->import();
-        $this->info('');
+    }
 
-        $this->info('Sync Tax:');
+    protected function syncTax()
+    {
         $this->wordpressService->setWooCommerceCallback('taxes');
         $this->wordpressService->import();
-        $this->info('');
+    }
 
-        $this->info('Sync Coupon:');
+    protected function syncCoupon()
+    {
         $this->wordpressService->setWooCommerceCallback('coupons');
         $this->wordpressService->import();
-        $this->info('');
+    }
 
-        $this->info('Sync products:');
+    protected function syncProduct()
+    {
         $this->wordpressService->inspector(function ($page) {
             $products = collect(Woocommerce::get('products', ['page' => $page]));
             $products->each(function ($product) {
