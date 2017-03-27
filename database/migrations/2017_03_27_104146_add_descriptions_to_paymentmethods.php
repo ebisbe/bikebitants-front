2a0ceb46df506550171d4dfef44f2736d3622547
Enric Bisbe Gil <enricu@gmail.com>
2017-03-27 15:54:28 +0200

    Remove n from text

--- database/migrations/2017_03_27_104146_add_descriptions_to_paymentmethods.php
+++ database/migrations/2017_03_27_104146_add_descriptions_to_paymentmethods.php
@@ -36,7 +36,7 @@ class AddDescriptionsToPaymentmethods extends Migration
             ->where('slug', 'bank-transfer')
             ->update([
                 'name' => 'Pagar via transferencia bancaria',
-                'description' => 'Por favor, realiza la transferencia bancaria usando el número de pedido como referencia de pago. Una vez realidada la transferencia, envia el comprobante de la transferencia al email <a href=\"mailto:hola@bikebitants.com\">hola@bikebitants.com</a>. Tu pedido no será enviado hasta que recibamos el comprobante de la transferencia.<br>\n<br>\nBanc Sabadell<br>\nNumero de cuenta<br>\n00815224470001279131<br>\nIBAN<br>\nES9700815224470001279131<br>\nBIC / Swift<br>\nBSAB ESBB'
+                'description' => "Por favor, realiza la transferencia bancaria usando el número de pedido como referencia de pago. Una vez realidada la transferencia, envia el comprobante de la transferencia al email <a href=\"mailto:hola@bikebitants.com\">hola@bikebitants.com</a>. Tu pedido no será enviado hasta que recibamos el comprobante de la transferencia.<br><br>Banc Sabadell<br>Numero de cuenta<br>00815224470001279131<br>IBAN<br>ES9700815224470001279131<br>BIC / Swift<br>BSAB ESBB"
             ]);
     }
 
