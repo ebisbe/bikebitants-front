2117a204360837ace04de94a485647eaa5ff1689
Enric Bisbe Gil <enricu@gmail.com>
2017-03-31 13:16:07 +0200

    Uncaught SyntaxError: Unexpected end of input - Remove comment

--- resources/views/scripts/addImpression.blade.php
+++ resources/views/scripts/addImpression.blade.php
@@ -6,7 +6,6 @@
         ga('ec:addProduct', {!! $product->gaProduct($view_name, $iteration) !!});
         ga('ec:setAction', 'click', {list: '{{ $view_name }}'});
 
-        // Send click with an event, then send user to product page.
         ga('send', 'event', 'UX', 'click', '{{ $view_name }}', {
             hitCallback: function () {
                 document.location = '{{ route('shop.slug', ['slug' => $product->slug]) }}';
