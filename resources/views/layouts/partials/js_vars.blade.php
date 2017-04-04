0e36232bd9e0a4b3844259606ce015306682a695
Enric Bisbe Gil <enricu@gmail.com>
2017-04-04 15:38:05 +0200

    Js Raven only on production

--- resources/views/layouts/partials/js_vars.blade.php
+++ resources/views/layouts/partials/js_vars.blade.php
@@ -1,6 +1,7 @@
 <script>
     window.Laravel = {!! json_encode([
             'csrfToken' => csrf_token(),
-            'language' => config('app.locale')
+            'language' => config('app.locale'),
+            'env' => config('app.env')
     ]) !!}
 </script>
