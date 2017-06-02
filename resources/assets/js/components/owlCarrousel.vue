9ca9c672e36a6bab0becfcb5b06fe1739cbd18cf
Enric Bisbe Gil <enricu@gmail.com>
2017-06-02 13:00:30 +0200

    Fix - showing image

--- resources/assets/js/components/owlCarrousel.vue
+++ resources/assets/js/components/owlCarrousel.vue
@@ -37,10 +37,8 @@
                 });
 
                 if (_.isEmpty(foundVariation)) {
-                    this.images.push(variation);
                     this.createNewVariation(variation);
                 }
-
 
                 location.hash = variation.file_hash;
             },
@@ -48,13 +46,13 @@
             createNewVariation: function (image) {
                 let imgTag = '<img data-src="/img/330/' + image.filename + '" data-sizes="100w" data-srcset="/img/330/' + image.filename + ' 360w,/img/450/' + image.filename + ' 480w,/img/538/' + image.filename + ' 568w,/img/355/' + image.filename + ' 1200w" class="img-responsive lazyloaded" sizes="100w" srcset="/img/330/' + image.filename + ' 360w,/img/450/' + image.filename + ' 480w,/img/538/' + image.filename + ' 568w,/img/355/' + image.filename + ' 1200w" src="/img/330/' + image.filename + '">';
                 if (typeof window.owl !== 'undefined') {
-                    console.log(this.images.length);
                     window.owl.trigger(
                         'add.owl.carousel', [
                             '<div data-hash="' + image.file_hash + '" class="item">' + imgTag + '</div>'
                             , 1
                         ]
                     );
+                    this.images.push(image);
                 }
             }
         }
